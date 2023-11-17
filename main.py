29b2354ade28aed29e7450dad9ca38b4a8d40bb5
edachedevops <victor.edache@binghamuni.edu.ng>
2023-11-17 13:52:19 +0100

    practice

---
+++ main.py
@@ -0,0 +1,4 @@
+print ("teacher: what is your name?")
+print ("victor: my name is edache victor")
+print ("teacher: alright, victor please define a noun")
+print ("victor: a noun is a name of a person, animal, places or things.")
